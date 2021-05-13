b34138c302857f4dc78a9edb7fbc91bc0c74e67a
unknown <gabiaronne@gmail.com>
2021-05-13 09:30:43 -0300

    pesquisas

--- BlogPessoal-Angular/src/app/inicio/inicio.component.ts
+++ BlogPessoal-Angular/src/app/inicio/inicio.component.ts
@@ -18,10 +18,13 @@ export class InicioComponent implements OnInit {
 
  postagem : Postagem = new Postagem()
  listaPostagens:Postagem[]
+ tituloPost:string
+ 
  tema:Tema = new Tema
  
 listaTemas:Tema[]
 idTema:number
+nomeTema:string
 
 usuario:Usuario = new Usuario
 idUser = environment.id
@@ -90,6 +93,28 @@ this.getAllPostagens()
   })
 }
 
+findByTituloPostagem(){
+  
+  if(this.tituloPost==''){
+    this.getAllPostagens()
+  } else{
+    this.postagemService.getByTituloPostagem(this.tituloPost).subscribe((resp:Postagem[])=>
+    {
+      this.listaPostagens=resp
+      })
+  }
+}
+findByNomeTema(){
+  if(this.nomeTema==''){
+ this.getAllTemas()
+}else{
+  this.temaService.getByNomeTema(this.nomeTema).subscribe((resp:tema[]) =>{
+this.listaTemas=resp
+
+  })
+
+}
 
 
+}
 }
